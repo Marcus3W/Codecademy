1e76e80560bbe44d5143d16932a7e88015577663
Marcus3W <marcus3w@live.com>
2022-01-31 22:15:50 +0000

    Finished whale talk finished

--- Full-Stack/JS_Syntax_II/JS_Arrays/whaleTalk.js
+++ Full-Stack/JS_Syntax_II/JS_Arrays/whaleTalk.js
@@ -1,23 +1,18 @@
 let input = 'turpentine and turtles';
 const vowels = ['a','e','i','o','u'];
 let resultArray = [];
-
 for(let i = 0; i < input.length; i++) {
     //console.log(i);
     for(let j = 0; j < vowels.length; j++) {
-
         if (input[i] === vowels[j]) {
-
             if (input[i] === 'e') {
                 resultArray.push('ee');
             }
             if (input[i] === 'u') {
                 resultArray.push('uu');
             }
-
             resultArray.push(input[i])
         }
     }
-
 }
 console.log(resultArray.join('').toUpperCase());
